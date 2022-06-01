88f57b327525fbcaf8e2d52851315ac2cd5ff284
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-01 09:16:51 -0600

    initial header component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Header from "./components/header/Header"; 
 
 function App() {
   return (
-    <div className="App">
-      Food Blog
-    </div>
+    <>
+      <Header/>
+      <p>test</p>
+    </>
   );
 }
 
