4df171bdfd5d084e4f93fc8d5498a5120fc09c17
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-02 09:17:57 -0600

    styling for publishing a new post complete

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,14 +3,25 @@ import "./write.css"
 export default function Write() {
     return (
         <div className="write">
+            <img
+                className= "writeImg" 
+                src="https://ifoodreal.com/wp-content/uploads/2020/11/Beets-with-Goat-Cheese-and-Pine-Nuts-709x1024.jpg" 
+                alt=""/>
             <form className="writeForm">
                 <div className="writeFormGroup">
                     <label htmlFor="fileInput">
                      <i class="fa-solid fa-plus"></i>
                     </label>
-                    <input type="file" id="fileInput"/>
-                    <input type="text" placeholder="Title"/>
+                    <input type="file" id="fileInput" style={{display:"none"}}/>
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
                 </div>
+                <div className="writeFormGroup">
+                    <textarea 
+                        placeholder="Start writing..." 
+                        type="text" 
+                        className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
             </form>
         </div>
     )
