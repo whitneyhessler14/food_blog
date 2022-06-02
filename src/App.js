1be66e2fae1a90894ef2de32247700419aaeac65
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-02 09:40:14 -0600

    Register and login pages created and styled with no function yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from "./components/header/Header"; 
-//import Home from "./pages/home/Home"
+import Home from "./pages/home/Home"
 import SinglePost from "./components/singlePost/SinglePost";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <>
       <Header/>
-      <Write />
+      <Register />
     </>
   );
 }
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,38 @@
+import "./settings.css";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsTitleUpdate">Update Your Account</span>
+          <span className="settingsTitleDelete">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://www.madd.org/wp-content/uploads/2019/02/blank-profile-picture-973460_640-500x500.png"
+              alt=""
+            />
+            <input
+              id="fileInput"
+              type="file"
+              style={{ display: "none" }}
+              className="settingsPPInput"
+            />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="User" name="name" />
+          <label>Email</label>
+          <input type="email" placeholder="User@user.mail" name="email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" name="password" />
+          <button className="settingsSubmitButton" type="submit">
+            Update
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
