f5961528754b855e03064cd40c1f186df6fee68d
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-03 09:05:52 -0600

    create and update user working

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected")).catch(err=>console.log(err));
 
 app.use("/back-end/auth", authRoute)
+app.use("/back-end/users", userRoute)
 
 app.listen("5000", () => {
     console.log("Back end is running on port 5000")
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -5,9 +5,17 @@ const bcrypt = require('bcrypt');
 //Update user information 
 router.put("/:id", async (req, res)=>{
     if(req.body.userID === req.params.id) {
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
         try{
-        
-        
+            const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+                $set: req.body,
+            },
+            { new: true}
+        );
+        res.status(200).json(updatedUser)
     }catch(err){
         res.status(500).json(err);
     }
