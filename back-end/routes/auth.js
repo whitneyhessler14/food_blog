013cf4ddd28097921e66352150be81eeae08af4c
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-03 08:44:30 -0600

    authentication set up completed

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
-const User = require("../models/User")
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register", async (req, res)=>{
     try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hashedPass,
         });
         const user = await newUser.save();
         res.status(200).json(user)
@@ -17,8 +20,20 @@ router.post("/register", async (req, res)=>{
 })
 
 //LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+            !user && res.status(400).json("User information not found")
 
+            const validated = await bcrypt.compare(req.body.password, user.password)
+            !validated && res.status(400).json("Incorrect Password")
 
-//POST
+            const {password, ...others} = user._doc;
+            res.status(200).json(others);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 
 module.exports = router
