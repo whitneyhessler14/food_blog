367a4f27b39da14f24a620230bc533df7f628349
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-03 08:19:18 -0600

    POST request for new user working

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected")).catch(err=>console.log(err));
+
+app.use("/back-end/auth", authRoute)
 
 app.listen("5000", () => {
     console.log("Back end is running on port 5000")
