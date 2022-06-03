834c60de8212b3b89ec94007df565cafee56d71b
Whitney Hessler <chenault.whitney@gmail.com>
2022-06-03 07:54:52 -0600

    User models created

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -5,5 +5,18 @@ const UserSchema = new mongoose.Schema({
         type:String,
         required:true,
         unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },password:{
+        type:String,
+        required:true
+    }, profilePicture:{
+        type:String,
+        default:""
     }
-})
+},{timestamps:true})
+
+module.exports = mongoose.model("User", UserSchema);
